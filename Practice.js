23ce46e2f4d1a17baf4367a754fdb43ba9c53548
kowais915 <kowais915@gmail.com>
2021-02-19 10:07:09 +0500

    practicing control structures

--- Practice.js
+++
@@ -1,3 +0,0 @@
-let name = 'Owais';
-
-console.log(name);
---
+++ controlFlow.js
@@ -0,0 +1,12 @@
+// Control Flow
+
+// For loop
+let names = ['Owais', 'Saad', 'Sikandar', 'Hamza', 'Ammad'];
+
+for(i=0; i<names.length; i++){
+
+    let html = `<div>${names[i]}</div>`;
+    console.log(html);
+}
+
+console.log('Loop finsihed');
