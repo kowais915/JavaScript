671929962c4fa713285d05d6dab60c88e78f4ea8
kowais915 <kowais915@gmail.com>
2021-02-19 09:35:57 +0500

    Practicing common array methods

--- Arrays.js
+++ Arrays.js
@@ -1,12 +1,22 @@
 // Array in JS
 
-let dreams = 'dfasfsdfa';
+// let dreams = 'dfasfsdfa';
 
-let result = dreams.replace('f', 'q');
+// let result = dreams.replace('f', 'q');
 
 
 
-let names = ['Owais', 'Saad', 'Sikandar', 'Hamza'];
+// let names = ['Owais', 'Saad', 'Sikandar', 'Hamza'];
 
-let check = names.includes('Hamza');
-console.log(check);
+// let check = names.includes('Hamza');
+// console.log(check);
+
+// == can't differentiate between a string and a number so it will identify '25' and 25 as the same variables while === is used for strict comparision which is to say it differentiats between strings and numbers.
+// let num1 = 24;
+
+// console.log(num1 == 24);
+// console.log(num1 == 243);
+// console.log(num1 == 9);
+
+let subject = 'jsfjsosajfio';
+console.log(subject.indexOf('f'));
