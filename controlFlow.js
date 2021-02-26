dbef6154bac12b2aa9f884b1e7ce847f7081f817
kowais915 <kowais915@gmail.com>
2021-02-26 13:54:08 +0500

    Practicing Arrays

--- controlFlow.js
+++ controlFlow.js
@@ -1,12 +1,43 @@
-// Control Flow
+// // let arr1 = ['owais', 'Saad', 'Ahmad', 'Ammad'];
 
-// For loop
-let names = ['Owais', 'Saad', 'Sikandar', 'Hamza', 'Ammad'];
+// // for(i = 0; i<arr1.length; i++){
+// //     console.log(arr1[i]);
+// // }
 
-for(i=0; i<names.length; i++){
+// // console.log('Loop is finished');
 
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
-}
+// // let name = 'jkdjsdlfjioesfoesjf';
 
-console.log('Loop finsihed');
+// // let result = name.lastIndexOf('e');
+// // console.log(result);
+
+// // let firstName = 'Owais';
+// // let lastName = 'Khan';
+
+// // console.log(firstName.concat(' Khan'));
+
+// //A loop with 10 ietrations
+
+// // for(i=0; i<10; i++){
+// //     console.log(`ietration ${i}`);
+// // }
+
+// // console.log('Loop finished');
+
+// let string1 = 'dsfkjdjsfsdf';
+// let result = string1.indexOf('s');
+// console.log(result);
+// let i = 20;
+// do{
+//     // console.log('this is an infinite loop');
+
+// // // } while(i<30);
+// let i = 3;
+// while(i<20){
+//     console.log('Hello Less Than 20!');
+//     i++;
+// }
+
+let num1 = '45';
+let result = Number(num1);
+console.log(34 +result);
