5289cc1646425dd27e0b2dbb2d0f8f3943272e9c
kowais915 <kowais915@gmail.com>
2021-09-03 15:44:20 +0500

    accessing children of an element via dom manipulation

--- RandomCodeJs/domMan.js
+++ RandomCodeJs/domMan.js
@@ -1,5 +1,14 @@
-// How do you change attribute of a node?
+// // How do you change attribute of a node?
 
-const para = document.querySelector('p');
+// const para = document.querySelector('p');
 
-para.setAttribute('style', 'color: green;');
+// para.setAttribute('style', 'color: green;');
+
+// Accessing children of a tag
+
+const tag = document.querySelector('div');
+console.log(tag.children);
+
+const converted = Array.from(tag.children);
+
+console.log(converted);
